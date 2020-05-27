8c8cab53f33513c935485a57b6146f074e434ba6
MZP <604183841@qq.com>
2020-05-27 20:11:39 +0800

    新增 元素定位搜索selenium

--- own/location_elements.py
+++ own/location_elements.py
@@ -6,5 +6,8 @@ myDriver = webdriver.Chrome('/Users/MZP/Desktop/chromedriver')
 myDriver.get_window_size()
 myDriver.get("http://www.baidu.com")
 
+myDriver.find_element_by_id("kw").send_keys('selenium')
+myDriver.find_element_by_id('su').click()
+
 time.sleep(5)
 myDriver.quit()
